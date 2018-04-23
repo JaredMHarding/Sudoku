9329b9ea162d3108a57ece5d5c2cc0c311c757e7
Jared Micheal Harding <jharding@ecs325-17.labs.encs>
2018-04-23 16:27:33 -0700

    Added resource files

--- Sudoku/ViewController.swift
+++ Sudoku/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var PuzzleView: SudokuView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
