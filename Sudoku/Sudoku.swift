fd7b2ec6c8e4280ee9748473d053841ff7d46f97
Jared Micheal Harding <jharding@ecs325-17.labs.encs>
2018-04-23 17:57:06 -0700

    Set up skeleton for Sudoku.swift code.

---
+++ Sudoku/Sudoku.swift
@@ -0,0 +1,33 @@
+//
+//  Sudoku.swift
+//  Sudoku
+//
+//  Created by Jared Micheal Harding on 4/23/18.
+//  Copyright © 2018 Jared Micheal Harding. All rights reserved.
+//
+
+import Foundation
+
+class Sudoku {
+    
+    func numberAt(row r: Int, column c: Int) -> Int {
+        return 0
+    }
+    
+    func numberIsFixedAt(row r: Int, column c: Int) -> Bool {
+        return false
+    }
+    
+    func isConflictingEntryAt(row r: Int, column c: Int) -> Bool {
+        return false
+    }
+    
+    func anyPencilSetAt(row r: Int, column c: Int) -> Bool {
+        return false
+    }
+    
+    func isSetPencil(_ n: Int, row r: Int, column c: Int) -> Bool {
+        return false
+    }
+    
+}
